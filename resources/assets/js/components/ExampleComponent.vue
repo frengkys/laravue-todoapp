e456af7f180d8aa306414cbe3ceee60411f894f1
Frengky <frengky@node8.net>
2018-02-05 12:54:43 +0700

    initial commit

---
+++ resources/assets/js/components/ExampleComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Example Component</div>
+
+                    <div class="panel-body">
+                        I'm an example component!
+                    </div>
+                    <table class="table">
+                    <tr>
+                        <td>test</td>
+                        <td>test</td>
+                    </tr>
+                    <tr>
+                        <td>blah blah</td>
+                        <td>blah blah</td>
+                    </tr>
+                    </table>
+                    <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component ready.')
+        }
+    }
+</script>
